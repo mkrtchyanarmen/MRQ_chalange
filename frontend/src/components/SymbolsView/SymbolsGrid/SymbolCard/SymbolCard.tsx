b020348534b212bacb3075371c15fd82f47c4784
Armen <armdevjs@gmail.com>
2023-09-20 17:44:25 +0400

    feat: stock price change animation

--- frontend/src/components/SymbolsView/SymbolsGrid/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolsView/SymbolsGrid/SymbolCard/SymbolCard.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 
 import { useAppSelector } from "@/hooks/redux";
 
@@ -6,28 +6,60 @@ import SymbolCardHeader from "./SymbolCardHeader";
 import SymbolCardBaseInfo from "./SymbolCardBaseInfo";
 
 import { roundNumber } from "./utils";
-
 import "./symbolCard.css";
 
 type SymbolCardProps = {
   id: string;
   onClick: (symbolId: string) => void;
-  price: number;
+  price?: number;
 };
 
 const SymbolCard: FC<SymbolCardProps> = ({ id, onClick, price }) => {
+  // Prev price to compare with changed one to see if the progress is positive or negative
+  const [prevPrice, setPrevPrice] = useState(price);
+
+  //Card refferance for addind and removing classnames conditionally
+  const ref = useRef<HTMLDivElement>(null);
+
   const {
     trend,
     industry,
     companyName: name,
     marketCap,
   } = useAppSelector((state) => state.stocks.entities[id]);
+
   const handleOnClick = useCallback(() => {
     onClick(id);
   }, [id]);
 
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    //Shake needs to happen only when the price is changed, when the prevPrice is null no need to shake
+    if (prevPrice && price) {
+      const shadowClassName =
+        prevPrice > price
+          ? "symbolCard__negativeShadow"
+          : "symbolCard__positiveShadow";
+      ref.current?.classList.add("symbolCard__shake", shadowClassName);
+
+      // Remove the className when animation is ended
+      timeout = setTimeout(() => {
+        ref.current?.classList.remove(
+          "symbolCard__shake",
+          "symbolCard__negativeShadow",
+          "symbolCard__positiveShadow"
+        );
+      }, 620);
+    }
+    setPrevPrice(price);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [price]);
+
   return (
-    <div onClick={handleOnClick} className="symbolCard">
+    <div ref={ref} onClick={handleOnClick} className="symbolCard">
       <SymbolCardHeader {...{ id, trend }} />
       <ul className="symbolCard__info">
         <li className="symbolCard__info__item">
